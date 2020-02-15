f738d0565ad2ee2d734f147b37c9ba6460a91d9a
kleg <contact@winnzos.fr>
2020-02-15 14:30:19 +0100

    Optimize

--- src/action/closestToZero.ts
+++ src/action/closestToZero.ts
@@ -13,34 +13,19 @@ export default function closestToZero(data: string): number {
       //Pour de la rapidité et des problèmes avec typeScript j'ai retiré la gestion des array multidimensions
       //const flatArray = array.flat();
 
-      //Classe la liste des entiers plus proches de zéro au plus grand
       array.sort((previous: number, current: number) => {
-        if (current < Math.abs(previous)) {
-          if (Math.abs(current) === Math.abs(previous)) {
-            return 1;
-          } else {
-            return 1;
-          }
-        } else {
-          return -1;
-        }
+        return Math.abs(previous) - Math.abs(current);
       });
 
       let numberFinal: number = array[0];
-      //Gestion si l'entier est négatif recherche d'un eventuel doublon positif
-      if (Math.sign(numberFinal) < 0) {
-        const positive: number | undefined = array.find(
-          current => current === Math.abs(numberFinal)
-        );
-        if (positive) {
-          return positive;
-        }
-      }
 
-      return numberFinal;
+      const positive: number | undefined = array.find(
+        current => current === Math.abs(numberFinal)
+      );
+
+      return positive ? positive : numberFinal;
     }
   }
 
-  //Retour 0 pour tous les autres cas
   return 0;
 }
